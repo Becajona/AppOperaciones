b08e9d0ddd2fc3258b37df218a34d5a22596716f
Jonathan <utp0155232@alumno.utpuebla.edu.mx>
2024-03-02 12:57:31 -0600

    funciones.jvg

---
+++ AppOperaciones/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AppOperaciones
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppOperaciones/Paginas/DiseñoGridLayout.xaml.cs
@@ -0,0 +1,18 @@
+using System;
+using Microsoft.Maui.Controls;
+
+namespace AppOperaciones.Paginas
+{
+    public partial class DiseñoGridLayout : ContentPage
+    {
+        public DiseñoGridLayout()
+        {
+            InitializeComponent();
+        }
+
+        private async void BtnIniciar_Clicked(object sender, EventArgs e)
+        {
+            // Tu lógica aquí
+        }
+    }
+}
